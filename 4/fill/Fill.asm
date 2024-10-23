0bc5160c24ea55d0e178d34a9373725973f0985c
Alec <alecschneider@me.com>
2024-10-23 15:20:23 -0700

    chapter 4 & 5 done

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,68 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+@R0
+// 0 is white
+M=0
+
+(LOOP)
+@KBD
+D=M
+@WHITE
+D;JEQ
+@BLACK
+0;JMP
+
+(WHITE)
+@R0
+D=M
+@DONE
+D;JEQ
+@R0
+M=0
+@R1
+M=0
+@DRAW
+0;JMP
+
+(BLACK)
+@R0
+D=M
+@DONE
+D;JGT
+@R0
+M=1
+@R1
+M=-1
+@DRAW
+0;JMP
+
+(DRAW)
+@SCREEN
+D=A
+@n
+M=D
+@i
+M=0
+(DRAWLOOP)
+  @8192
+  D=A
+  @i
+  D=M-D
+  @LOOP
+  D;JEQ
+  @R1
+  D=M
+  @n
+  A=M
+  M=D
+  @n
+  M=M+1
+  @i
+  M=M+1
+  @DRAWLOOP
+  0;JMP
+
+(DONE)
+@LOOP
+0;JMP
